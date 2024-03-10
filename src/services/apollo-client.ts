a0edb28bf080799204f57b13f56bfeb4fa4b1eee
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-03-10 23:04:30 +0100

    Allow Apollo logging

--- src/services/apollo-client.ts
+++ src/services/apollo-client.ts
@@ -1,7 +1,25 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+
+const httpLink = new HttpLink({
+  uri: `https://graphql.prepr.io/${process.env.PREPR_ACCESS_TOKEN}`,
+});
+
+const logLink = new ApolloLink((operation, forward) => {
+  const shouldLog = process.env.PREPR_GRAPHQL_LOGGING === "true";
+  shouldLog && console.log("Request started! Query:", operation);
+  return forward(operation).map((response) => {
+    shouldLog && console.log("Response received!", response);
+    return response;
+  });
+});
 
 const client = new ApolloClient({
-  uri: `https://graphql.prepr.io/${process.env.PREPR_ACCESS_TOKEN}`,
+  link: ApolloLink.from([logLink, httpLink]),
   cache: new InMemoryCache(),
 });
 

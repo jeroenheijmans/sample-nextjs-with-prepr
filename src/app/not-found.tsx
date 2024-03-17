9d6b984c8de9a8267cad03ed87da5f3fadfbe2af
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-03-17 16:16:01 +0100

    Add some default routing stuff

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+export default function ContactPage() {
+  return (
+    <main className="w-full pb-8">
+      <div className="my-8 mx-auto max-w-screen-lg px-4">
+        <article className="prose">
+          <h1>💔 Four, oh four 🔍</h1>
+          <p>
+            <strong>The page you were looking for was not found.</strong> It can
+            be because the page was never created of course, as this is just a
+            sample app.
+          </p>
+          <p>Try again later, please!?</p>
+        </article>
+      </div>
+    </main>
+  );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,12 @@ export default function Header() {
       <div className="mx-auto max-w-screen-lg py-1 px-4 flex flex-wrap gap-2 sm:gap-8">
         <div className="flex flex-wrap gap-2">
           <MenuMainItem href="/">Home</MenuMainItem>
-          <MenuMainItem href="/">Blog</MenuMainItem>
-          <MenuMainItem href="/">Technologies</MenuMainItem>
-          <MenuMainItem href="/">Contact</MenuMainItem>
+          <MenuMainItem href="/articles">Articles</MenuMainItem>
+          <MenuMainItem href="/contact">Contact</MenuMainItem>
         </div>
         <div className="sm:ml-auto flex flex-wrap gap-2">
-          <MenuMainItem href="/">Sign In</MenuMainItem>
-          <MenuMainItem href="/">Search</MenuMainItem>
+          <MenuMainItem href="/sign-in">Sign In</MenuMainItem>
+          <MenuMainItem href="/search">Search</MenuMainItem>
         </div>
       </div>
     </header>

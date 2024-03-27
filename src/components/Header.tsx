25834ea4e3c862de44c7c41c2712df744cf8a038
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-03-27 22:08:18 +0100

    Make menu open on hover (mobile view temporarily broken now)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,7 @@ export default async function Header() {
 
   return (
     <header className="bg-pink-900 text-pink-100 w-full md:sticky top-0 drop-shadow-lg z-10">
-      <div className="mx-auto max-w-screen-lg py-1 px-4 flex flex-wrap gap-2 md:gap-8 flex-col md:flex-row">
+      <div className="mx-auto max-w-screen-lg md:px-4 flex flex-wrap gap-2 md:gap-4 flex-col md:flex-row">
         {navigation.menu_items.map((menuItem: any) => (
           <MenuMainItem key={menuItem.title} item={menuItem} />
         ))}
--- src/components/MenuMainItem.tsx
+++ src/components/MenuMainItem.tsx
@@ -12,10 +12,10 @@ export default function MenuMainItem({ item }: MenuMainItemProps) {
   const { href, title, isActive, isExternal } = useExtractedLink(item);
 
   return (
-    <div className="flex flex-col grow">
+    <div className="relative group">
       <Link
-        className={`py-2 sm:py-4 hover:underline hover:text-pink-50 text-xl ${
-          isActive ? "underline" : ""
+        className={`inline-block z-0 py-2 px-4 group-hover:text-pink-50 group-hover:bg-pink-800 text-lg ${
+          isActive ? "bg-pink-800" : ""
         }`}
         target={isExternal ? "_blank" : "_self"}
         href={href}
@@ -23,7 +23,7 @@ export default function MenuMainItem({ item }: MenuMainItemProps) {
         {title} {isExternal && <sup className="text-xs">↗</sup>}
       </Link>
       {item.children.length > 0 && (
-        <ul>
+        <ul className="hidden absolute z-10 min-w-64 p-4 bg-pink-800 border border-pink-900 border-t-0 drop-shadow-xl group-hover:block">
           {item.children.map((child: any, index: number) => (
             <MenuChildItem key={index} {...child} />
           ))}

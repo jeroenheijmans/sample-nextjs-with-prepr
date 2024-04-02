65b3e4e1b6754c623e4e24ad565ce7c6a4a01bad
Jeroen Heijmans <github@jeroenheijmans.nl>
2024-04-02 18:26:20 +0200

    Fix auto-replace mistake

--- src/components/CmsTextBlock.tsx
+++ src/components/CmsTextBlock.tsx
@@ -2,7 +2,7 @@ import CmsHtml from "./CmsHtml";
 
 export default function CmsTextBlock({ data }: { data: any }) {
   return (
-    <div className={`page-content`}>
+    <div className="page-content">
       <CmsHtml html={data.content} listStyleImage={data.list_style_image} />
     </div>
   );
